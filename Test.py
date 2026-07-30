ec51f51d4042eff150623957d223af0ea1715ecf
Aftabahmed <aftabsattar361@gmail.com>
2026-07-30 14:49:59 +0500

    added Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,28 @@
-print("Hello, GitHub!")
+import streamlit as st
 
-name = input("Enter your name: ")
+# Page Settings
+st.set_page_config(
+    page_title="Login Form",
+    page_icon="🔐"
+)
 
-print(f"Welcome, {name}!")
-print("This is my first Python program on GitHub.")
+# Title
+st.title("🔐 Login Form")
+
+# Username & Password
+username = st.text_input("Username")
+
+password = st.text_input(
+    "Password",
+    type="password"
+)
+
+# Login Button
+if st.button("Login"):
+
+    if username == "admin" and password == "12345":
+        st.success("✅ Login Successful")
+        st.write("Welcome Admin")
+
+    else:
+        st.error("❌ Invalid Username or Password")
